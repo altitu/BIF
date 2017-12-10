75a26a3d32f5912535bff0e494ee122a02244b01
Altitu <alan.turpin51@gmail.com>
2017-12-10 01:16:51 +0100

    added verbosity option for openFasta fuction, with tests

--- filemanager.py
+++ filemanager.py
@@ -2,37 +2,39 @@
 # -*- coding: utf-8 -*-
 
 
-def openFasta(name):
-	#on parse le nom et on affiche le type supposé de fichier
-	pos = -1
-	posbar = -1
-	ln = len(name)
-	for l in range(ln-1, -1, -1):
-		if (name[l] == '.' and (pos == -1)):
-			#print "name: "+name[l]
-			pos = l+1
-			#print "pos: "+str(pos)
-		if (name[l] == '/' and (posbar == -1)):
-			#print "name: "+name[l]
-			posbar = l+1
-			#print "posbar: "+str(posbar)
-	#nom sans slash
-	nameWObar = name[posbar:ln]
+def openFasta(name, verbosity):
 
-	print("reading "+nameWObar) 
-	#print name[pos:ln]
-	if (name[pos: ln] == "fasta" or name[pos: ln] == "fas" or name[pos: ln] == "fa"):
-		print("(generic FASTA file)")
-	elif (name[pos: ln] == "fna"):
-		print("(fasta nucleic acid)")
-	elif (name[pos: ln] == "ffn"):
-		print("(fasta functional nucleotide)")
-	elif (name[pos: ln] == "faa"):
-		print("(fasta amino acid)")
-	elif (name[pos: ln] == "frn"):
-		print("(fasta RNA non-coding)")
-	else:
-		print ("(assuming generic FASTA file)")	
+	if (verbosity > 0):
+		#on parse le nom et on affiche le type supposé de fichier
+		pos = -1
+		posbar = -1
+		ln = len(name)
+		for l in range(ln-1, -1, -1):
+			if (name[l] == '.' and (pos == -1)):
+				#print "name: "+name[l]
+				pos = l+1
+				#print "pos: "+str(pos)
+			if (name[l] == '/' and (posbar == -1)):
+				#print "name: "+name[l]
+				posbar = l+1
+				#print "posbar: "+str(posbar)
+		#nom sans slash
+		nameWObar = name[posbar:ln]
+
+		print("reading "+nameWObar) 
+		#print name[pos:ln]
+		if (name[pos: ln] == "fasta" or name[pos: ln] == "fas" or name[pos: ln] == "fa"):
+			print("(generic FASTA file)")
+		elif (name[pos: ln] == "fna"):
+			print("(fasta nucleic acid)")
+		elif (name[pos: ln] == "ffn"):
+			print("(fasta functional nucleotide)")
+		elif (name[pos: ln] == "faa"):
+			print("(fasta amino acid)")
+		elif (name[pos: ln] == "frn"):
+			print("(fasta RNA non-coding)")
+		else:
+			print ("(assuming generic FASTA file)")	
 	#we attempt to read the file
 	try:
 		ffile = open(name, 'r')
@@ -85,140 +87,140 @@ def openFasta(name):
 	if (flushed == False): #si on ne termine pas par un '\n'
 		reads.append(read)
 		flushed = True
-
-	#on parse pour estimer la provenance de la BDD
-	alreadySaid = False
-	#db = identifiers[0].split('|')
-	db = identifiers
-	#print("db:: "+str(db))
-	if (len(db) == 1):
-		if (alreadySaid == False):
-			print "warning: cannot find NCBI sequence identifier"
-			print "\tswitching to second parsing mode"
-			alreadySaid = True
-		db = identifiers[0].split(' ')
-		if len(db) > 1:
-			print ("identifier: "+db[0])
-			if ((db[1][0] == '[') and (db[1][len(db[1])-1] == ']')):
-				line = db[1].split('=')
-				try:
-					print(line[0][1:len(line[0])] +":\n\t" + line[1][0:len(line[1]-1)])
+	if (verbosity > 1):
+		#on parse pour estimer la provenance de la BDD
+		alreadySaid = False
+		#db = identifiers[0].split('|')
+		db = identifiers
+		#print("db:: "+str(db))
+		if (len(db) == 1):
+			if (alreadySaid == False):
+				print "warning: cannot find NCBI sequence identifier"
+				print "\tswitching to second parsing mode"
+				alreadySaid = True
+			db = identifiers[0].split(' ')
+			if len(db) > 1:
+				print ("identifier: "+db[0])
+				if ((db[1][0] == '[') and (db[1][len(db[1])-1] == ']')):
+					line = db[1].split('=')
+					try:
+						print(line[0][1:len(line[0])] +":\n\t" + line[1][0:len(line[1]-1)])
+						ending = ""
+						for i in range(2, len(line)):
+							ending += line[i] + " "
+						print(ending)
+					except:
+						pass
+				else:
 					ending = ""
-					for i in range(2, len(line)):
-						ending += line[i] + " "
-					print(ending)
-				except:
-					pass
+					for i in range(1, len(db)):
+							ending += db[i] + " "
+					print("\t"+ending+"\n")
 			else:
-				ending = ""
-				for i in range(1, len(db)):
-						ending += db[i] + " "
-				print("\t"+ending+"\n")
+				print "\tsecond parsing mode failed, sequence:"
+				print str(db)+"\n"
 		else:
-			print "\tsecond parsing mode failed, sequence:"
-			print str(db)+"\n"
-	else:
-		recognized = True
-		strings = []
-		if(db[0] == "bbs" and len(db) > 1):
-			strings.append("GenInfo Backbone Id")
-			strings.append("Number: "+str(db[1]))
-			strings.append("comment:")
-			for i in range(2, len(db)):
-				if (db[i] != ""):
-					strings.append("\t"+db[i])
-		elif(db[0] == "dbj" and len(db) > 2):
-			strings.append("DDBJ, DNA Database of Japan")
-			strings.append("accession: "+str(db[1]))
-			strings.append("locus: "+str(db[2]))
-			strings.append("comment:")
-			for i in range(3, len(db)):
-				if (db[i] != ""):
-					strings.append("\t"+db[i])
-		elif(db[0] == "emb" and len(db) > 2):
-			strings.append("EMBL Data Library")
-			strings.append("accession: "+str(db[1]))
-			strings.append("locus: "+str(db[2]))
-			strings.append("comment:")
-			for i in range(3, len(db)):
-				if (db[i] != ""):
-					strings.append("\t"+db[i])
-		elif(db[0] == "gb" and len(db) > 2):
-			strings.append("GenBank")
-			strings.append("accession: "+str(db[1]))
-			strings.append("locus: "+str(db[2]))
-			strings.append("comment:")
-			for i in range(3, len(db)):
-				if (db[i] != ""):
-					strings.append("\t"+db[i])
-		elif(db[0] == "gnl" and len(db) > 2):
-			strings.append("General database identifier")
-			strings.append("database: "+str(db[1]))
-			strings.append("identifier: "+str(db[2]))
-			strings.append("comment:")
-			for i in range(3, len(db)):
-				if (db[i] != ""):
-					strings.append("\t"+db[i])
-		elif(db[0] == "lcl" and len(db) > 1):
-			strings.append("Local Sequence identifier")
-			strings.append("identifier: "+str(db[1]))
-			strings.append("comment:")
-			for i in range(2, len(db)):
-				if (db[i] != ""):
-					strings.append("\t"+db[i])
-		elif(db[0] == "pat" and len(db) > 2):
-			strings.append("Patents")
-			strings.append("country: "+str(db[1]))
-			strings.append("number: "+str(db[2]))
-			strings.append("comment:")
-			for i in range(3, len(db)):
-				if (db[i] != ""):
-					strings.append("\t"+db[i])
-		elif(db[0] == "pdb" and len(db) > 2):
-			strings.append("Brookhaven Protein Data Bank")
-			strings.append("entry: "+str(db[1]))
-			strings.append("chain: "+str(db[2]))
-			strings.append("comment:")
-			for i in range(3, len(db)):
-				if (db[i] != ""):
-					strings.append("\t"+db[i])
-		elif(db[0] == "pir" and len(db) > 2):
-			strings.append("NBRF PIR")
-			strings.append("entry: "+str(db[2]))
-			strings.append("comment:")
-			for i in range(3, len(db)):
-				if (db[i] != ""):
-					strings.append("\t"+db[i])
-		elif(db[0] == "prf" and len(db) > 2):
-			strings.append("Protein Research Foundation")
-			strings.append("name: "+str(db[2]))
-			strings.append("comment:")
-			for i in range(3, len(db)):
-				if (db[i] != ""):
-					strings.append("\t"+db[i])
-		elif(db[0] == "ref" and len(db) > 2):
-			strings.append("NCBI Reference Sequence")
-			strings.append("accession: "+str(db[1]))
-			strings.append("locus: "+str(db[2]))
-			strings.append("comment:")
-			for i in range(3, len(db)):
-				if (db[i] != ""):
-					strings.append("\t"+db[i])
-		elif(db[0] == "sp" and len(db) > 2):
-			strings.append("SWISS-PROT")
-			strings.append("accession: "+str(db[1]))
-			strings.append("entry name: "+str(db[2]))
-			strings.append("comment:")
-			for i in range(3, len(db)):
-				if (db[i] != ""):
-					strings.append("\t"+db[i])
-		else:
-			print "unrecognize db provenance"
-			recognized = False
+			recognized = True
+			strings = []
+			if(db[0] == "bbs" and len(db) > 1):
+				strings.append("GenInfo Backbone Id")
+				strings.append("Number: "+str(db[1]))
+				strings.append("comment:")
+				for i in range(2, len(db)):
+					if (db[i] != ""):
+						strings.append("\t"+db[i])
+			elif(db[0] == "dbj" and len(db) > 2):
+				strings.append("DDBJ, DNA Database of Japan")
+				strings.append("accession: "+str(db[1]))
+				strings.append("locus: "+str(db[2]))
+				strings.append("comment:")
+				for i in range(3, len(db)):
+					if (db[i] != ""):
+						strings.append("\t"+db[i])
+			elif(db[0] == "emb" and len(db) > 2):
+				strings.append("EMBL Data Library")
+				strings.append("accession: "+str(db[1]))
+				strings.append("locus: "+str(db[2]))
+				strings.append("comment:")
+				for i in range(3, len(db)):
+					if (db[i] != ""):
+						strings.append("\t"+db[i])
+			elif(db[0] == "gb" and len(db) > 2):
+				strings.append("GenBank")
+				strings.append("accession: "+str(db[1]))
+				strings.append("locus: "+str(db[2]))
+				strings.append("comment:")
+				for i in range(3, len(db)):
+					if (db[i] != ""):
+						strings.append("\t"+db[i])
+			elif(db[0] == "gnl" and len(db) > 2):
+				strings.append("General database identifier")
+				strings.append("database: "+str(db[1]))
+				strings.append("identifier: "+str(db[2]))
+				strings.append("comment:")
+				for i in range(3, len(db)):
+					if (db[i] != ""):
+						strings.append("\t"+db[i])
+			elif(db[0] == "lcl" and len(db) > 1):
+				strings.append("Local Sequence identifier")
+				strings.append("identifier: "+str(db[1]))
+				strings.append("comment:")
+				for i in range(2, len(db)):
+					if (db[i] != ""):
+						strings.append("\t"+db[i])
+			elif(db[0] == "pat" and len(db) > 2):
+				strings.append("Patents")
+				strings.append("country: "+str(db[1]))
+				strings.append("number: "+str(db[2]))
+				strings.append("comment:")
+				for i in range(3, len(db)):
+					if (db[i] != ""):
+						strings.append("\t"+db[i])
+			elif(db[0] == "pdb" and len(db) > 2):
+				strings.append("Brookhaven Protein Data Bank")
+				strings.append("entry: "+str(db[1]))
+				strings.append("chain: "+str(db[2]))
+				strings.append("comment:")
+				for i in range(3, len(db)):
+					if (db[i] != ""):
+						strings.append("\t"+db[i])
+			elif(db[0] == "pir" and len(db) > 2):
+				strings.append("NBRF PIR")
+				strings.append("entry: "+str(db[2]))
+				strings.append("comment:")
+				for i in range(3, len(db)):
+					if (db[i] != ""):
+						strings.append("\t"+db[i])
+			elif(db[0] == "prf" and len(db) > 2):
+				strings.append("Protein Research Foundation")
+				strings.append("name: "+str(db[2]))
+				strings.append("comment:")
+				for i in range(3, len(db)):
+					if (db[i] != ""):
+						strings.append("\t"+db[i])
+			elif(db[0] == "ref" and len(db) > 2):
+				strings.append("NCBI Reference Sequence")
+				strings.append("accession: "+str(db[1]))
+				strings.append("locus: "+str(db[2]))
+				strings.append("comment:")
+				for i in range(3, len(db)):
+					if (db[i] != ""):
+						strings.append("\t"+db[i])
+			elif(db[0] == "sp" and len(db) > 2):
+				strings.append("SWISS-PROT")
+				strings.append("accession: "+str(db[1]))
+				strings.append("entry name: "+str(db[2]))
+				strings.append("comment:")
+				for i in range(3, len(db)):
+					if (db[i] != ""):
+						strings.append("\t"+db[i])
+			else:
+				print "unrecognize db provenance"
+				recognized = False
 
-		if (recognized == True):
-			for i in strings:
-				print(i)
+			if (recognized == True):
+				for i in strings:
+					print(i)
 
 	return (identifiers, reads)
 			
@@ -228,7 +230,13 @@ def openFasta(name):
 	#on retournera un tableau de read dans 0 et de commentaire dans 1: (read, comment)*
 
 def main():
-	print openFasta("./test_perso/test_dbPRF.fasta")
+	print "verbosity: 0"
+	print openFasta("./test_perso/test_dbPRF.fasta", 0)
+	print "verbosity: 1"
+	print openFasta("./test_perso/test_dbPRF.fasta", 1)
+	print "verbosity: 2"
+	print openFasta("./test_perso/test_dbPRF.fasta", 2)
+
 
 if (__name__ == '__main__'):
 	main()
